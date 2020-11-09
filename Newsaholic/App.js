15331eda4ccff4ae798a314cd3f116a278259736
sayanmondal2098@gmail.com <sayanmondal2098@gmail.com>
2020-11-09 21:22:01 +0530

    login register added

--- Newsaholic/App.js
+++ Newsaholic/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/navigations/Navigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      hIIIII
-    </View>
+    <AppNavigator/>
   );
 }
 
